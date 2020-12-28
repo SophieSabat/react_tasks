be6314fce284c95dbce8300363b8e29a1a416af7
SophieSabat <sonyasab2000@gmail.com>
2020-12-28 21:13:40 +0200

    task 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, {Component} from 'react';
-import {cars} from "./database/carsDB";
-import CarComponents from "./components/car/CarComponents";
+import AllPostsComponents from "./components/allPosts/AllPostsComponents";
 
 class App extends Component {
     render() {
         return (
             <div>
-                {
-                    cars.map(car => <CarComponents item={car} key={car.id}/>)
-                }
+                <AllPostsComponents/>
             </div>
         );
     }
