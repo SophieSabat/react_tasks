cb3c62f102a2531deacc8f48aa3d04b23c83b8ad
SophieSabat <sonyasab2000@gmail.com>
2021-01-05 23:50:00 +0200

    task 8, part 1

--- src/App.js
+++ src/App.js
@@ -1,147 +1,36 @@
-import React, {useState, useEffect, useReducer} from 'react';
+import React, {memo, useState, useCallback, useMemo} from 'react';
 import './App.css'
 
 
 
-// const Test = () => {
-//     useEffect(() => {
-//         console.log('mount');
-//
-//
-//         return () => {
-//             console.log('unmount');
-//         }
-//     }, []);
-//     return (
-//         <h1>Will be unmounted</h1>
-//     )
-// }
+const Test = memo(({onClick, cartSum}) => {
+    console.log('test was rerender');
 
+    return <h2 onClick={() => onClick(Math.random())}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium est facilis id omnis quidem sequi? Ab assumenda facere molestias nobis pariatur! A aspernatur deleniti eligendi ipsam molestiae quisquam sequi totam!
+        {cartSum.val}
+    </h2>
+});
 
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_TODO_ITEM' : {
-            return action.payload;
-        }
-        case 'RESET_TODO' : {
-            return null;
-        }
-        case 'TOGGLE_TODO_COMPLETED' : {
-            return {
-                ...state,
-                completed: !state.completed
-            };
-        }
-        case 'CHANGE_TODO_TITLE' : {
-            return {
-                ...state,
-                title: action.payload
-            };
-        }
-        default: {
-            console.error('not valid action', action.type);
-            return state;
-        }
-    }
-}
-const initialState = null;
 
 
 export default function App() {
 
-    const [counter, setCounter] = useState(1);
-    // const [user, setUser] = useState(null);
-    // const [todo, setTodo] = useState(null);
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [counter, setCounter] = useState(0);
+    const [arr, setArr] = useState([1,3,432,12,457,2321,112,44,2,124,554,3998]);
 
-    const [inputValue, inputChange] = useState('');
+    const onClickHandler = useCallback((num) => console.log(num), []);
 
-
-
-    // const fetchData = async () => {
-    //     const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`).then(value => value.json());
-    //     setTodo(response);
-    // }
-
-    const fetchData = async () => {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`).then(value => value.json());
-        dispatch({type: 'SET_TODO_ITEM', payload: response});
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [counter]);
-
-
-
-
-
-    const handleInc = () => {
-        setCounter((prevState) => prevState + 1);
-    }
-    // const handleReset = () => {
-    //     setCounter(1);
-    // }
-    // const handleSetUser = () => {
-    //     setUser({
-    //         name: 'test',
-    //         age: 23
-    //     });
-    // }
-    // const handleResetUser = () => {
-    //     setUser(null);
-    // }
-
-    // const handleResetTodo = () => {
-    //     setTodo(null);
-    // }
-
-    const handleResetTodo = () => {
-        dispatch({type: 'RESET_TODO'});
-    }
-
-    const handleTodoCompleted = () => {
-        dispatch({type: 'TOGGLE_TODO_COMPLETED'});
-    }
-
-    const handleTodoTitle = () => {
-        dispatch({type: 'CHANGE_TODO_TITLE', payload: inputValue});
-    }
-
-
+    const cartSum = useMemo(() => ({val: arr.reduce((acc, el) => (acc += el), 0)}),
+        [arr]
+    );
 
 
     return (
-        <div className={'app'}>
-            <h1>Counter: {counter}</h1>
-
-            {
-                // counter !== 2 && <Test/>
-            }
-
-            <button onClick={handleInc}>inc</button>
-            {/*<button onClick={handleReset}>reset</button>*/}
-
-            {/*<button onClick={handleSetUser}>set user</button>*/}
-            {/*<button onClick={handleResetUser}>reset user</button>*/}
-            {/*{*/}
-            {/*    user && <h2>{user.name} - {user.age}</h2>*/}
-            {/*}*/}
-
-
-            {/*<button onClick={handleResetTodo}>reset todo</button>*/}
-            {/*{*/}
-            {/*    todo && <h2>{todo.id} - {todo.title} - {todo.completed.toString()}</h2>*/}
-            {/*}*/}
-            <button onClick={handleResetTodo}>reset todo</button>
-            <button onClick={handleTodoCompleted}>toggle</button>
-
-            <input type="text" value={inputValue} onChange={({target: {value}}) => inputChange(value)}/>
-            <button onClick={handleTodoTitle}>change title</button>
-
-            {
-                state && <h2>{state.id} - {state.title} - {state.completed.toString()}</h2>
-            }
-        </div>
-    )
+      <div className={'app'}>
+          {/*<h1 onClick={() => setCounter((prev) => prev + 1)}>Hello World! {counter}</h1>*/}
+          <h1 onClick={() => setArr((prev) => [...prev, Math.random()])}>Hello World! {counter}</h1>
+          <Test title={'test'} onClick={onClickHandler} cartSum={cartSum}/>
+      </div>
+    );
 }
